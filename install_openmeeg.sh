01d14515fd83e987b448765769fa37e463c9a9b4
lazyparser <lazyparser@gmail.com>
2015-04-22 10:33:34 +0800

    install_openmeeg.sh: add comments and test codes.

--- install_openmeeg.sh
+++ install_openmeeg.sh
@@ -6,10 +6,13 @@ if [ $UID -ne 0 ]; then
 echo "Run me as root."
 exit 1
 fi
-
+n_cores=`cat /proc/cpuinfo | grep cores | wc -l`
 # Check if openMEEG has been installed.
 [ -f /usr/local/bin/om_assemble ] && exit 0
 
+# download it from github using zip, since this is more
+# stable than git-related-methods.
+# Life in mainland China, as a programmer, is not easy :-(
 wget 'https://github.com/openmeeg/openmeeg/archive/master.zip'
 	-O openmeeg-master.zip
 unzip openmeeg-master.zip
@@ -21,10 +24,13 @@ cmake -DBUILD_TESTING=ON \
 	-DUSE_PROCESSBAR=ON \
 	-DENABLE_PYTHON=ON \
 	-DUSE_OMP=ON ..
-make -j 12
-#make est / check
+make -j $n_cores
+# make est / check
 make install
 # fix ldconfig
 grep -q '/usr/local/lib64' /etc/ld.so.conf || echo '/usr/local/lib64/' >> /etc/ld.so.conf
 ldconfig
+
+# test it
+om_assemble
 
