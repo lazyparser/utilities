27a27c315b94aecc1b04ee4e65835d1417df722e
Wei Wu <lazyparser@gmail.com>
2018-06-14 10:54:02 +0800

    add deploy-aosp-env-trusty.sh

---
+++ deploy-aosp-env-trusty.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
+wget 'https://raw.githubusercontent.com/lazyparser/utilities/master/deploy-gitlab-runner.sh'
+bash deploy-gitlab-runner.sh
